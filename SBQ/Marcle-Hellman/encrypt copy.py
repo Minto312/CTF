72338efcfe943f197ae57d4e6c498d583ff363db
Minto312 <karinto3219@gmail.com>
2023-10-17 06:48:01 +0900

    Marcle-Hellman

--- SBQ/Marcle-Hellman/encrypt copy.py
+++ SBQ/Marcle-Hellman/encrypt copy.py
@@ -12,25 +12,30 @@ def encrypt():
     assert flag[-1] == '}'
     fb = ''
     for c in flag:
-        fb += '%08d' % (int(bin(ord(c))[2:]))
+        print(int(bin(ord(c))[:]))
+        # fb += '%08d' % (int(bin(ord(c))[2:]))
+        # print(fb)
+        
     n = len(fb)
     w = [randint(1, 10)]
     for i in range(1, n):
         w.append(randint(sum(w[:i]) + 1, w[-1] * 3))
     w = [n % p for n in w]
-    print('p =', p)
-    print('w =', w)
+    # print('p =', p)
+    # print('w =', w)
 
     c = 0
     # print(fb)
     for i, b in enumerate(fb):
         c += int(b) * w[i] % p
         c %= p
-        print(c)
+        # print(c)
     print('c =', c)
 
 def decrypt():
-    
+    pass
+
+
 if __name__ == '__main__':
     flag = 'CyberQuest{M4rcl3_H3llm4n_1s_4_5up3r_5tr0ng_5y5t3m}'
     p = 2021110809000000000000000000000000000000000000000000000000283
